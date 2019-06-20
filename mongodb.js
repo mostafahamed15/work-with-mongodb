f39ad5218d13cb4778c9d5d49fa15ba359235b3a
Mostafa <mostafa.hamed@linux-plus.com>
2019-06-20 17:10:48 +0200

    ObjectID

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,20 @@
 //CRUD create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+//Destructuring 
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+
+//Generate id with mongodb
+const id = new ObjectID()
+console.log(id)
+console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, {useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -12,8 +22,8 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true }, (error, client) => 
     }
     
     const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        name: "Mostafa",
-        age: 27
-    })
+    // db.collection('users').insertOne({
+    //     name: "Mostafa",
+    //     age: 27
+    // })
 })
