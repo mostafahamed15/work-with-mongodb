33d7e5cdf3965fe13676f4e6c0894bab99d68e56
Mostafa <mostafa.hamed@linux-plus.com>
2019-07-01 17:57:08 +0200

    Work with validators

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,16 +8,40 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        require: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        require: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error("Email is invalid")
+            }
+        }
+    },
+    password: {
+        type: String,
+        minlength: 6,
+        trim: true
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error("Age must be positive!")
+            }
+        }
     }
 })
 
 const me = new User ({
-    name: "Mostafa",
-    age: 27
+    name: "  Mostafa  ",
+    email: "  FF@GG.COM  "
 })
 
 me.save().then(() => {
@@ -24,3 +49,29 @@ me.save().then(() => {
 }).catch((error) => {
     console.log('Error!',error)
 })
+
+const Tasks = mongoose.model('Tasks', {
+    description: {
+        type: String,
+        require: true
+    },
+    completed: {
+        type: Boolean,
+        validate (value) {
+            if (typeof value !== Boolean) {
+                throw new Error('Value must be boolean true or false!')
+            }
+        }
+    }
+})
+
+// const task = new Tasks({
+//     description: "The first task",
+//     completed: true
+// })
+
+// task.save().then(() => {
+//     console.log(task)
+// }).catch((error) => {
+//     console.log('Error!',error)
+// })
