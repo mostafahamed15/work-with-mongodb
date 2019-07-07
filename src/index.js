bb8f604be5cba1329f374d16b7b3534d74845162
Mostafa <mostafa.hamed@linux-plus.com>
2019-07-07 16:44:37 +0200

    Use Resource creation endpoint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-require('./src/db/mongoose');
-const User = require('./src/models/user')
+require('./db/mongoose');
+const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express();
 const port = process.env.port | 3000;
@@ -17,6 +18,18 @@ app.post('/users', (req, res) => {
         res.status(400).send(e)
 
     })
+})
+
+app.post('/tasks', (req, res) => {
+
+    const task = new Task(req.body)
+
+    task.save().then(()=> {
+        res.send(task)
+    }).catch((e)=> {
+        res.status(400).send(e)
+    })
+    
 })
 
 app.listen(port, () => {
